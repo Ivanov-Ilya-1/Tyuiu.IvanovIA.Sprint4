20d4957c505fb68db986385c448d3258fe1eef1c
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-13 20:16:01 +0300

    Task6

---
+++ Tyuiu.IvanovIA.Sprint4.Task6.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IvanovIA.Sprint4.Task6.V7.Lib;
+namespace Tyuiu.IvanovIA.Sprint4.Task6.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] array = { "Математика", "Физика", "Химия", "Биология", "География", "История", "Экономика" };
+            int res = ds.Calculate(array);
+            int wait = 3;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
