1d7fcec238f0e6215cfd7f362ba847dea1202f6d
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-13 19:32:49 +0300

    Task4

---
+++ Tyuiu.IvanovIA.Sprint4.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.IvanovIA.Sprint4.Task4.V8.Lib;
+namespace Tyuiu.IvanovIA.Sprint4.Task4.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] nums = new int[5, 5]
+            {   { 1, 3, 2, 2, 5 },
+                { 6, 1, 5, 5, 2 },
+                { 1, 5, 4, 1, 4 },
+                { 4, 1, 4, 4, 5 },
+                { 4, 6, 1, 1, 4 } };
+
+            int[,] wait = new int[5, 5]
+            {   { 1, 3, 1, 1, 5 },
+                { 1, 1, 5, 5, 1 },
+                { 1, 5, 1, 1, 1 },
+                { 1, 1, 1, 1, 5 },
+                { 1, 1, 1, 1, 1 } };
+
+
+
+            int[,] res = ds.Calculate(nums);
+
+            CollectionAssert.AreEqual(nums, res);
+
+
+
+
+
+        }
+    }
+}
